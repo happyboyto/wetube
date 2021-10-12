c2e2ab70d499f4fab489fb538ec38849c3a58411
happyboyto <happyboyto95@gmail.com>
2021-10-13 00:59:14 +0900

    very tiny changing

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from "express";
 const app = express();
 const PORT = 4000;
 
+app.get("/", () => console.log("somebody is trying to go home!"))
+
 const handelListening = () => console.log(`server listening on port https://localhost:${PORT}`);
 
 app.listen(PORT, handelListening);
