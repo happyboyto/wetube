70d205a2234171599911e3d81be9582998f42581
happyboyto <54832160+happyboyto@users.noreply.github.com>
2021-10-13 18:04:03 +0900

    Day2
    only summary file is updated

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 import express from "express";
+import morgan from "morgan";
+
 const app = express();
+const morganMiddleware = morgan("dev");
 const PORT = 4000;
 
-app.get("/", () => console.log("somebody is trying to go home!"))
+app.use(morganMiddleware);
+app.get("/", () => console.log("somebody is trying to go home!"));
 
 const handelListening = () => console.log(`server listening on port https://localhost:${PORT}`);
 
